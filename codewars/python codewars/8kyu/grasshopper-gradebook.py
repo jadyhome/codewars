f946eea70c967de06348666376a85584c042c8e7
Jady Hom <hyjady@gmail.com>
2021-01-25 18:55:11 -0500

    grasshopper-gradebook python codewars

---
+++ codewars/python codewars/8kyu/grasshopper-gradebook.py
@@ -0,0 +1,36 @@
+########################################################################
+# PROMPT
+# https://www.codewars.com/kata/55cbd4ba903825f7970000f5/train/python
+
+# Complete the function so that it finds the mean of the three scores passed to it
+# and returns the letter value associated with that grade.
+
+# Numerical Score	    Letter Grade
+# 90 <= score <= 100	'A'
+# 80 <= score < 90	    'B'
+# 70 <= score < 80	    'C'
+# 60 <= score < 70	    'D'
+# 0 <= score < 60	    'F'
+
+# Tested values are all between 0 and 100.
+# Theres is no need to check for negative values or values greater than 100.
+########################################################################
+
+
+####### SOLUTION #######
+
+def get_grade(s1, s2, s3):
+    grades = int(s1 + s2 + s3) / 3
+
+    if 90 <= grades <= 100:
+        return "A"
+    elif 80 <= grades < 90:
+        return "B"
+    elif 70 <= grades < 80:
+        return "C"
+    elif 60 <= grades < 70:
+        return "D"
+    else:
+        return "F"
+
+####### SOLUTION #######
