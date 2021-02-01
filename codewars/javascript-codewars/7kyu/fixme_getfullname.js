337d5f9daa7c10c3298a398b080e76fb1a746fad
Jady Hom <hyjady@gmail.com>
2021-02-01 17:50:08 -0500

    fixme_getfullname javascript codewars

---
+++ codewars/javascript-codewars/7kyu/fixme_getfullname.js
@@ -0,0 +1,51 @@
+////////////////////////////////////////////////////////////////////////
+// PROMPT
+// https://www.codewars.com/kata/597c684822bc9388f600010f/train/javascript
+
+// The code provided is supposed return a person's Full Name given their first and last names.
+
+// But it's not working properly.
+
+// Notes
+// The first and/or last names are never null, but may be empty.
+
+// Task
+// Fix the bug so we can all go home early.
+////////////////////////////////////////////////////////////////////////
+
+
+/////// QUESTION ///////
+
+class Dinglemouse{
+  constructor( firstName, lastName ){
+  }
+  
+  getFullName(){
+    return firstName+" "+lastName
+  }
+}
+
+/////// QUESTION ///////
+
+
+
+/////// SOLUTION ///////
+
+class Dinglemouse {
+  constructor(firstName, lastName) {
+    this.firstName = firstName,
+    this.lastName = lastName
+  }
+
+  getFullName() {
+    if ( this.firstName !== "" && this.lastName !== "" ) {
+      return `${this.firstName} ${this.lastName}`
+    } else if ( this.firstName === "" && this.lastName === "" ) {
+      return ""
+    } else {
+      return this.firstName || this.lastName
+    }
+  }
+}
+
+/////// SOLUTION ///////
