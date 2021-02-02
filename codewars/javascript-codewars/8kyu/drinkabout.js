58c6a06b17653edab4cab2687bfcb00cf05e1f7e
Jady Hom <hyjady@gmail.com>
2021-02-02 17:38:42 -0500

    drinkabout javascript codewars

---
+++ codewars/javascript-codewars/8kyu/drinkabout.js
@@ -0,0 +1,40 @@
+////////////////////////////////////////////////////////////////////////
+// PROMPT
+// https://www.codewars.com/kata/56170e844da7c6f647000063/train/javascript
+
+// Kids drink toddy.
+// Teens drink coke.
+// Young adults drink beer.
+// Adults drink whisky.
+// Make a function that receive age, and return what they drink.
+
+// Rules:
+// Children under 14 old.
+// Teens under 18 old.
+// Young under 21 old.
+// Adults have 21 or more.
+
+// Examples:
+// peopleWithAgeDrink(13) === "drink toddy"
+// peopleWithAgeDrink(17) === "drink coke"
+// peopleWithAgeDrink(18) === "drink beer"
+// peopleWithAgeDrink(20) === "drink beer"
+// peopleWithAgeDrink(30) === "drink whisky"
+////////////////////////////////////////////////////////////////////////
+
+
+/////// SOLUTION ///////
+
+function peopleWithAgeDrink(old) {
+  if (old < 14) {
+    return "drink toddy";
+  } else if (old < 18) {
+    return "drink coke";
+  } else if (old < 21) {
+    return "drink beer";
+  } else {
+    return "drink whisky";
+  }
+}
+
+/////// SOLUTION ///////
