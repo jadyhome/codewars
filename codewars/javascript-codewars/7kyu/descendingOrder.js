d106df90b0e8f16f8f8283635ebe194e5ea82777
Jady Hom <hyjady@gmail.com>
2021-02-11 16:58:20 -0500

    descendingOrder javascript codewars

---
+++ codewars/javascript-codewars/7kyu/descendingOrder.js
@@ -0,0 +1,27 @@
+////////////////////////////////////////////////////////////////////////
+// PROMPT
+// https://www.codewars.com/kata/5467e4d82edf8bbf40000155/train/javascript
+
+// Your task is to make a function that can take any non-negative integer as an argument
+// and return it with its digits in descending order.
+// Essentially, rearrange the digits to create the highest possible number.
+
+// Examples:
+// Input: 42145         Output: 54421
+
+// Input: 145263        Output: 654321
+
+// Input: 123456789     Output: 987654321
+////////////////////////////////////////////////////////////////////////
+
+
+/////// SOLUTION ///////
+
+function descendingOrder(n) {
+  let nums = n.toString()
+  let numbers = nums.split("")
+  let sortedStr = numbers.sort((a, b) => b - a)
+  return parseInt(sortedStr.join(""))
+}
+
+/////// SOLUTION ///////
