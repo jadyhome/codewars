f9feb588e10239ade634d7f467556c19ecfeeef9
Jady Hom <hyjady@gmail.com>
2021-03-30 17:34:23 -0400

    mumbling -- codewars

---
+++ codewars/javascript-codewars/7kyu/Mumbling.js
@@ -0,0 +1,27 @@
+////////////////////////////////////////////////////////////////////////
+// PROMPT
+// https://www.codewars.com/kata/5667e8f4e3f572a8f2000039/train/javascript
+
+// This time no story, no theory. The examples below show you how to write function accum:
+
+// Examples:
+
+// accum("abcd") -> "A-Bb-Ccc-Dddd"
+// accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+// accum("cwAt") -> "C-Ww-Aaa-Tttt"
+// The parameter of accum is a string which includes only letters from a..z and A..Z.
+////////////////////////////////////////////////////////////////////////
+
+
+/////// SOLUTION ///////
+
+function accum(s) {
+  let mumbling = s.split("");
+
+  mumbling = mumbling.map(
+    (char, index) => char.toUpperCase() + char.toLowerCase().repeat(index)
+  );
+  return mumbling.join("-");
+}
+
+/////// SOLUTION ///////
