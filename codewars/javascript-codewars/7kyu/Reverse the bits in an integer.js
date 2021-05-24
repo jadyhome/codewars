0b7b7e41519b5e126d76744f01473d2978284a47
Jady Hom <hyjady@gmail.com>
2021-05-24 16:21:00 -0400

    reverse the bits in an integer -- codewars

---
+++ codewars/javascript-codewars/7kyu/Reverse the bits in an integer.js
@@ -0,0 +1,19 @@
+////////////////////////////////////////////////////////////////////////
+// PROMPT
+// https://www.codewars.com/kata/5959ec605595565f5c00002b/train/javascript
+
+// Write a function that reverses the bits in an integer.
+// For example, the number 417 is 110100001 in binary.
+// Reversing the binary is 100001011 which is 267.
+// You can assume that the number is not negative.
+////////////////////////////////////////////////////////////////////////
+
+
+/////// SOLUTION ///////
+
+function reverseBits(n) {
+  let bin = n.toString(2).split("").reverse().join("");
+  return parseInt(bin, 2);
+}
+
+/////// SOLUTION ///////
