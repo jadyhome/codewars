6b5d19f242133827358230d1feca7c273c6845e0
Jady Hom <hyjady@gmail.com>
2021-05-25 17:41:14 -0400

    recycle! -- codewars

---
+++ codewars/javascript-codewars/7kyu/Recycle!.js
@@ -0,0 +1,30 @@
+////////////////////////////////////////////////////////////////////////
+// PROMPT
+// https://www.codewars.com/kata/59fb783bab11f89202001083/train/javascript
+
+// You are having a BBQ, after the BBQ you are left with the rubbish. You have 3 recycling boxes:
+// Red: Plastics, Green: Glass, Blue: Card.
+
+// You will need to sort the rubbish according to the material and
+// return the number of items in each box in the form of an array
+// i.e [2,3,4]
+// where 2 is the number of plastic items, 3 is the number of glass items and 4 is the number of card items.
+
+// assume:
+// Plastics > 0
+// Glass < 0
+// Card = 0
+////////////////////////////////////////////////////////////////////////
+
+
+/////// SOLUTION ///////
+
+function recycleMe(recycle) {
+  let plastic = recycle.filter((p) => p > 0).length;
+  let glass = recycle.filter((g) => g < 0).length;
+  let card = recycle.filter((c) => c == 0).length;
+
+  return [plastic, glass, card];
+}
+
+/////// SOLUTION ///////
