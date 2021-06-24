a0d962995da16d939af51f305521e5f688ef6d79
Jady Hom <hyjady@gmail.com>
2021-06-24 16:30:19 -0400

    the ladies of eniac -- codewars

---
+++ codewars/javascript-codewars/7kyu/The Ladies of ENIAC.js
@@ -0,0 +1,31 @@
+////////////////////////////////////////////////////////////////////////
+// PROMPT
+// https://www.codewars.com/kata/56d31aaefd3a52902a000d66/train/javascript
+
+// Comprised of a team of five incredibly brilliant women, "The ladies of ENIAC" were the first “computors”
+// working at the University of Pennsylvania’s Moore School of Engineering (1945).
+// Through their contributions, we gained the first software application and the first programming classes!
+// The ladies of ENIAC were inducted into the WITI Hall of Fame in 1997!
+
+// Write a function which reveals "The ladies of ENIAC" names,
+// so that you too can add them to your own hall of tech fame!
+
+// To keep: only alpha characters, space characters and exclamation marks.
+// To remove: numbers and these characters: %$&/£?@
+
+// Result should be all in uppercase.
+////////////////////////////////////////////////////////////////////////
+
+
+/////// SOLUTION ///////
+
+function radLadies(name) {
+  let ladiesOfENIAC = name
+    .match(/[a-z! ]/gi)
+    .map((l) => l.toUpperCase())
+    .join("");
+
+  return ladiesOfENIAC;
+}
+
+/////// SOLUTION ///////
