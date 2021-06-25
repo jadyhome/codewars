df25b4b4f19d80108beaffc414cfef6f8f782ed5
Jady Hom <hyjady@gmail.com>
2021-06-25 17:18:56 -0400

    test's results -- codewars

---
+++ codewars/javascript-codewars/7kyu/Test's results.js
@@ -0,0 +1,48 @@
+////////////////////////////////////////////////////////////////////////
+// PROMPT
+// https://www.codewars.com/kata/599db0a227ca9f294b0000c8/train/javascript
+
+// It's important day today: the class has just had a math test.
+// You will be given a list of marks. Complete the function that will:
+
+// Calculate the average mark of the whole class and round it to 3 decimal places.
+// Make a dictionary/hash with keys "h", "a", "l" to make clear how many high, average and low marks they got.
+// High marks are 9 & 10, average marks are 7 & 8, and low marks are 1 to 6.
+// Return list [class_average, dictionary] if there are different type of marks,
+// or [class_average, dictionary, "They did well"] if there are only high marks.
+
+// Examples
+// [10, 9, 9, 10, 9, 10, 9] ==> [9.429, {'h': 7, 'a': 0, 'l': 0}, 'They did well']
+// [5, 6, 4, 8, 9, 8, 9, 10, 10, 10] ==> [7.9, {'h': 5, 'a': 2, 'l': 3}]
+////////////////////////////////////////////////////////////////////////
+
+
+/////// SOLUTION ///////
+
+function testResult(array) {
+  let dictionary = { h: 0, a: 0, l: 0 };
+
+  for (let i = 0; i <= array.length; i++) {
+    if (array[i] > 8) {
+      dictionary["h"]++;
+    }
+    if (array[i] > 6 && array[i] < 9) {
+      dictionary["a"]++;
+    }
+    if (array[i] < 7) {
+      dictionary["l"]++;
+    }
+  }
+
+  let avg = Number(
+    (array.reduce((a, b) => a + b, 0) / array.length).toFixed(3)
+  );
+
+  if (dictionary["a"] === 0 && dictionary["l"] === 0) {
+    return [avg, dictionary, "They did well"];
+  } else {
+    return [avg, dictionary];
+  }
+}
+
+/////// SOLUTION ///////
