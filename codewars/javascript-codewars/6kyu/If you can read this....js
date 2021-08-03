d2f764402622b1b19b0efe9e243d0b59439d5ba6
Jady Hom <hyjady@gmail.com>
2021-08-03 16:29:58 -0400

    if you can read this... -- codewars

---
+++ codewars/javascript-codewars/6kyu/If you can read this....js
@@ -0,0 +1,33 @@
+////////////////////////////////////////////////////////////////////////
+// PROMPT
+// https://www.codewars.com/kata/586538146b56991861000293/train/javascript
+
+// Task
+// You'll have to translate a string to Pilot's alphabet (NATO phonetic alphabet).
+
+// Input:
+// If, you can read?
+
+// Output:
+// India Foxtrot , Yankee Oscar Uniform Charlie Alfa November Romeo Echo Alfa Delta ?
+
+// Note:
+// There are preloaded dictionary you can use, named NATO
+// The set of used punctuation is ,.!?.
+// Punctuation should be kept in your return string, but spaces should not.
+// Xray should not have a dash within.
+// Every word and punctuation mark should be seperated by a space ' '.
+// There should be no trailing whitespace
+////////////////////////////////////////////////////////////////////////
+
+
+/////// SOLUTION ///////
+
+function to_nato(words) {
+  words = words.toLowerCase().replace(/\s/g, "").split("");
+
+  let pilot = words.map((l) => (l = NATO[l] ? NATO[l] : l)).join(" ");
+  return pilot;
+}
+
+/////// SOLUTION ///////
